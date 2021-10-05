485eeef97aa373c8c12f84a51c63feddb1908779
Aravindhan V <aravindhan.v@perfios.com>
2021-10-05 19:52:17 +0530

    ADDED: logic to generate 5 3-digit numbers

---
+++ IfElse/MinMax.js
@@ -0,0 +1,8 @@
+let min = Number.MIN_VALUE;
+let max = Number.MAX_VALUE;
+const NO_OF_INPUTS = 5;
+
+for (let input = 0; input < NO_OF_INPUTS; input++) {
+    let number = Math.floor(Math.random() * 900) + 100;
+    console.log(number);
+}
