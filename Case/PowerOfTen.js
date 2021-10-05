6d5858f31ec4a5219cb3cc1cb3e239170fa0ddc5
Aravindhan V <aravindhan.v@perfios.com>
2021-10-05 21:52:56 +0530

    ADDED: logic to take number and print power of ten in words using switch-case

---
+++ Case/PowerOfTen.js
@@ -0,0 +1,17 @@
+let number = 100;
+
+switch (number) {
+    case 1: console.log("One");
+        break;
+
+    case 10: console.log("Ten");
+        break;
+
+    case 100: console.log("Hundred");
+        break;
+
+    case 1000: console.log("Thousand");
+        break;
+
+    default: console.log("Invalid Input");
+}
