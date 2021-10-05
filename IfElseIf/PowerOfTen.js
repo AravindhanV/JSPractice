b49a723a50d6f69a5ade908c3bfa1b5dedab41eb
Aravindhan V <aravindhan.v@perfios.com>
2021-10-05 21:33:40 +0530

    ADDED: logic to accept number and print the power of ten

---
+++ IfElseIf/PowerOfTen.js
@@ -0,0 +1,13 @@
+let number = 100;
+
+if (number == 1) {
+    console.log("Units");
+} else if (number == 10) {
+    console.log("Tens");
+} else if (number == 100) {
+    console.log("Hundreds");
+} else if (number == 1000) {
+    console.log("Thousands");
+} else {
+    console.log("Invalid Input");
+}
