0487b4d88f5799a51d799f1345a981ab557aa1cd
Aravindhan V <aravindhan.v@perfios.com>
2021-10-05 19:54:48 +0530

    ADDED: logic to find least and greatest numbers

--- IfElse/MinMax.js
+++ IfElse/MinMax.js
@@ -1,8 +1,12 @@
-let min = Number.MIN_VALUE;
-let max = Number.MAX_VALUE;
+let min = Number.MAX_VALUE;
+let max = Number.MIN_VALUE;
 const NO_OF_INPUTS = 5;
 
 for (let input = 0; input < NO_OF_INPUTS; input++) {
     let number = Math.floor(Math.random() * 900) + 100;
+    min = Math.min(min, number);
+    max = Math.max(max, number);
     console.log(number);
 }
+
+console.log("MIN: " + min + ", MAX: " + max);
