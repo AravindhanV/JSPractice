bdb2a5d5317ab0b091020767c4a864a682ec9b64
Aravindhan V <varavindhan2010@gmail.com>
2021-10-06 19:52:44 +0530

    ADDED: storing the generated numbers in an array

--- Array/ThreeDigitNumber.js
+++ Array/ThreeDigitNumber.js
@@ -1,6 +1,8 @@
 const NO_OF_NUMBERS = 10;
+let numbers = new Array();
 
 for (let count = 0; count < NO_OF_NUMBERS; count++) {
   let number = Math.floor(Math.random() * 900) + 100;
-  console.log(number);
+  numbers.push(number);
+  console.log(numbers);
 }
