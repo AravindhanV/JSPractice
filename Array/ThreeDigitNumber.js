5fd8da1174c1a27cfca01dfe8f7cac04e288bfd3
Aravindhan V <varavindhan2010@gmail.com>
2021-10-06 20:02:25 +0530

    ADDED: implementations to find second largest and smallest elements in array

--- Array/ThreeDigitNumber.js
+++ Array/ThreeDigitNumber.js
@@ -4,5 +4,24 @@ let numbers = new Array();
 for (let count = 0; count < NO_OF_NUMBERS; count++) {
   let number = Math.floor(Math.random() * 900) + 100;
   numbers.push(number);
-  console.log(numbers);
 }
+
+let numbersCopy = [...numbers];
+let max = numbersCopy.reduce((a, b) => {
+  return Math.max(a, b);
+});
+numbersCopy.splice(numbersCopy.indexOf(max), 1);
+max = numbersCopy.reduce((a, b) => {
+  return Math.max(a, b);
+});
+console.log("Second Largest " + max);
+
+numbersCopy = [...numbers];
+let min = numbersCopy.reduce((a, b) => {
+  return Math.min(a, b);
+});
+numbersCopy.splice(numbersCopy.indexOf(min), 1);
+min = numbersCopy.reduce((a, b) => {
+  return Math.min(a, b);
+});
+console.log("Second Smallest " + min);
