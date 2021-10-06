8592b75d5a0594909f1ae0331f525bab032d2229
Aravindhan V <varavindhan2010@gmail.com>
2021-10-06 21:02:09 +0530

    ADDED: stopping criteria to know when to stop rolling the die

--- Map/Dice.js
+++ Map/Dice.js
@@ -1,6 +1,9 @@
 let dieRolls = {};
-
+let roll = -1;
 while (true) {
-  let roll = Math.floor(Math.random() * 6) + 1;
+  roll = Math.floor(Math.random() * 6) + 1;
   dieRolls[roll] = 1 + (dieRolls[roll] ? dieRolls[roll] : 0);
+  if (dieRolls[roll] == 10) {
+    break;
+  }
 }
